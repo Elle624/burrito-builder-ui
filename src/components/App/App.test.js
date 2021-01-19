47ed747200617ad4712216115ea46fabde593a61
Yiran Li <yiran.li@hotmail.com>
2021-01-19 11:00:25 -0700

    Initial setup for App testing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+import { getOrders, addOrder } from '../../apiCalls';
+jest.mock('../../apiCalls');
 
+describe('App Component', () => {
+  beforeEach(() => {});
+
+  it('should render correctly', () => {});
+
+  it('should be able to add new order', () => {});
+
+  it('should only be able to add new order when name and at least one ingredient is selected', () => {});
+});
