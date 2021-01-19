b0870cdad11dcae9bf0515a2155699082917160f
Yiran Li <yiran.li@hotmail.com>
2021-01-19 11:42:08 -0700

    Add more mock ata

--- src/components/App/_mockData.js
+++ src/components/App/_mockData.js
@@ -8,5 +8,13 @@ export const _mockOrders = {
 export const _mockNewOrder = {
   id: 333,
   name: 'Elle',
-  ingredients: ['beans', 'steak']
+  ingredients: ['cilantro', 'guacamole']
+};
+
+export const _mockUpdatedOrders = {
+  orders: [
+    { id: 111, name: 'Pam', ingredients: ['beans', 'lettuce', 'carnitas'] },
+    { id: 222, name: 'Leta', ingredients: ['steak', 'pico de gallo'] },
+    { id: 333, name: 'Elle', ingredients: ['cilantro', 'guacamole'] }
+  ]
 };
