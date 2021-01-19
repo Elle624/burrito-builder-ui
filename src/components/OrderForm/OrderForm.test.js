eeef23a9a047fc6cdc434031c75edeea4316807e
Yiran Li <yiran.li@hotmail.com>
2021-01-19 10:24:22 -0700

    Initial setup for form testing

---
+++ src/components/OrderForm/OrderForm.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import OrderForm from '../OrderForm';
+
+describe('OrderForm Component', () => {
+  it('should render correctly');
+
+  it('should change name upon inputting');
+
+  it('should reflect ingredients names upon selecting');
+});
