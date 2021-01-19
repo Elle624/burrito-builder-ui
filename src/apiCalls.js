f41ca3ae7bd2a0174c237c4fc4dc3348ffc77dce
Yiran Li <yiran.li@hotmail.com>
2021-01-19 10:08:44 -0700

    Add function for POST

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,23 @@
 export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
-}
+  return fetch('http://localhost:3001/api/v1/orders').then((response) =>
+    response.json()
+  );
+};
+
+export const addOrder = (data) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(
+        'Sorry we are having difficulty processing this request, please try again later!'
+      );
+    }
+  });
+};
